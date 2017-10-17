fb0cdfb167429780d75756b6ac48c5a2afb09101
KlimentS <kliment@mit.edu>
2017-10-17 11:54:50 -0400

    Initial Commit

---
+++ vEB_binary_tree_layout/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  vEB_binary_tree_layout
+//
+//  Created by Kliment Serafimov on 10/17/17.
+//  Copyright © 2017 Kliment Serafimov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
